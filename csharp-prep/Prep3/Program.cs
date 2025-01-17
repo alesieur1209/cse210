6aeed84c2cc2737b49d40b6e3c6a78b59efe69a7
alesieur1209 <alesieur1209@gmail.com>
2025-01-16 22:12:04 -0700

    did prep assignments 3 4 and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 50);
+
+        int guess = -1;
+
+        while (guess != secretNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (secretNumber > guess)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (secretNumber < guess)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+
+        }                    
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int enteredNumber = -1;
+        while (enteredNumber != 0)
+        {
+            Console.Write("Enter a new number, or enter 0 to quit:");
+            
+            string userResponse = Console.ReadLine();
+            enteredNumber = int.Parse(userResponse);
+            
+            if (enteredNumber != 0)
+            {
+                numbers.Add(enteredNumber);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"Your total is {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"Your average is {average}");
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"Your max is {max}");
     }
 }
